38a0143dcf2f3443a7174b793f9a36c0749f7b8a
Inna-ID <gmccsilv@gmail.com>
2019-05-01 22:30:10 +0300

    Change tasks table to simple list..., add task, team pages..., add layout folder and change direction of some components

--- src/Ziro-front-end/src/components/Layout/Content/index.jsx
+++ src/Ziro-front-end/src/components/Layout/Content/index.jsx
@@ -1,18 +1,23 @@
 import React, { Component } from 'react';
 import { Switch, Route } from 'react-router-dom'
-import Home from '../Home'
-import Authorization from '../Authorization'
-import Profile from '../Profile'
-import NotFound from '../NotFound'
+import Tasks from '../../Tasks'
+import Task from '../../Task'
+import Team from '../../Team'
+import Authorization from '../../Authorization'
+import Profile from '../../Profile'
+import NotFound from '../../NotFound'
 
 class Content extends Component {
     render() {
         return (
             <main>
                 <Switch>
-                    <Route exact path="/" component={Home} />
+                    <Route exact path="/" component={Tasks} />
                     <Route path="/profile" component={Profile}/>
+                    <Route path="/team" component={Team}/>
                     <Route path="/authorization" component={Authorization} />
+
+                    <Route path="/task" component={Task} />
 
                     <Route component={NotFound} />
                 </Switch>
