2a0eed6be2e43792f3b6436f8de773bea19a3974
Inna-ID <gmccsilv@gmail.com>
2019-05-11 18:11:50 +0300

    Add edit profile form

---
+++ src/Ziro/Ziro.Web/wwwroot/react-dev/components/Profile/ProfileEditForm.jsx
@@ -0,0 +1,207 @@
+import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+import Paper from '@material-ui/core/Paper';
+import Typography from '@material-ui/core/Typography';
+import Grid from '@material-ui/core/Grid';
+import TextField from '@material-ui/core/TextField';
+import Button from '@material-ui/core/Button';
+import InputLabel from '@material-ui/core/InputLabel';
+import MenuItem from '@material-ui/core/MenuItem';
+import FormControl from '@material-ui/core/FormControl';
+import Select from '@material-ui/core/Select';
+import { DropzoneArea } from 'material-ui-dropzone'
+import withStyles from '@material-ui/core/styles/withStyles';
+//import './adminpanel.css'
+import { isUserAuthenticated } from '../../utils.js'
+
+const styles = theme => ({
+   root: {
+      display: 'flex',
+      flexWrap: 'wrap',
+      marginTop: '30px'
+   },
+   paper: {
+      marginTop: theme.spacing.unit * 3,
+      marginBottom: theme.spacing.unit * 3,
+      padding: theme.spacing.unit * 2,
+      [theme.breakpoints.up(600 + theme.spacing.unit * 3 * 2)]: {
+         width: 900,
+         marginLeft: 'auto',
+         marginRight: 'auto',
+         marginTop: theme.spacing.unit * 6,
+         marginBottom: theme.spacing.unit * 6,
+         padding: theme.spacing.unit * 3,
+      },
+   },
+   formControl: {
+      margin: theme.spacing.unit,
+      minWidth: 120,
+   },
+   selectEmpty: {
+      marginTop: theme.spacing.unit * 2,
+   },
+   avatar: {
+      margin: theme.spacing.unit,
+      backgroundColor: theme.palette.secondary.main,
+   },
+});
+
+class ProfileEditForm extends Component {
+   constructor(props) {
+	   super(props);
+
+	   if (!isUserAuthenticated()) {
+		   this.props.history.push('/authorization');
+		   return;
+	   }
+
+      this.state = {
+         files: [],
+         position: '',
+      };
+   }
+
+   handleChange = event => {
+      this.setState({ [event.target.name]: event.target.value });
+   };
+
+   handleUpload(files) {
+      this.setState({
+         files: files
+      });
+   }
+
+   render() {
+      const { classes } = this.props;
+      return (
+         <div className="container">
+            <Paper className={classes.paper}>
+               <Typography component="h1" variant="h5" align="center">Редактирование профиля</Typography>
+               <form className={`${classes.root} edit-form`} action="">
+                  <Grid className="grid__container" container spacing={24}>
+                     <Grid className="grid__item" item xs={12} sm={6}>
+                        <TextField
+                           required
+                           id="firstName"
+                           name="firstName"
+                           label="Имя"
+                           fullWidth
+                           autoComplete="fname"
+                           defaultValue={this.props.data.name}
+                        />
+                     </Grid>
+                     <Grid className="grid__item" item xs={12} sm={6}>
+                        <TextField
+                           required
+                           id="lastName"
+                           name="lastName"
+                           label="Фамилия"
+                           fullWidth
+                           autoComplete="lname"
+                           defaultValue={this.props.data.lastName}
+                        />
+                     </Grid>
+                     <Grid className="grid__item" item xs={12} sm={6}>
+                        <TextField                           
+                           id="patronymic"
+                           name="patronymic"
+                           label="Отчество"
+                           fullWidth
+                           autoComplete="patronymic"
+                        />
+                     </Grid>
+                     <Grid className="grid__item" item xs={12} sm={6}>
+                        <TextField
+                           required
+                           id="email"
+                           name="email"
+                           label="Email"
+                           type="email"
+                           fullWidth
+                           autoComplete="email"
+                           defaultValue={this.props.data.email}
+                        />
+                     </Grid>
+                     <Grid className="grid__item" item xs={12} sm={6}>
+                        <TextField                           
+                           id="skype"
+                           name="skype"
+                           label="Skype"
+                           fullWidth
+                           autoComplete="skype"
+                           defaultValue={this.props.data.skype}
+                        />
+                     </Grid>
+                     <Grid className="grid__item" item xs={12} sm={6}>
+                        <TextField
+                           required
+                           id="phone"
+                           name="phone"
+                           label="Номер телефона"
+                           type="phone"
+                           fullWidth
+                           autoComplete="phone"
+                           defaultValue={this.props.data.phoneNumber}
+                        />
+                     </Grid>
+                     <Grid className="grid__item" item xs={12} sm={6}>
+                        <FormControl 
+                           required
+                           className={classes.formControl}
+                           fullWidth
+                           >
+                           <InputLabel htmlFor="position-simple">Должность</InputLabel>
+                           <Select
+                              value={this.state.position}
+                              onChange={this.handleChange}
+                              inputProps={{
+                                 name: 'position',
+                                 id: 'position-simple',
+                              }}
+                           >
+                              <MenuItem value="">
+                                 <em>None</em>
+                              </MenuItem>
+                              <MenuItem value={101}>Backend developer</MenuItem>
+                              <MenuItem value={102}>ASP.NET developer</MenuItem>
+                              <MenuItem value={103}>Frontend developer</MenuItem>
+                              <MenuItem value={104}>Frontend (React) developer</MenuItem>
+                              <MenuItem value={105}>IOS developer</MenuItem>
+                           </Select>
+                        </FormControl>
+                     </Grid>
+                     <Grid className="grid__item" item xs={12} sm={6}>
+                        <TextField
+                           required
+                           id="date"
+                           label="Дата рождения"
+                           type="text"
+                           fullWidth
+                           // InputLabelProps={{
+                           //    shrink: true,
+                           // }}
+                           defaultValue={this.props.data.dateOfBirth}
+                        />
+                     </Grid>
+                     <Grid className="grid__item upload__item" item xs={12} sm={6}>
+                     <Typography className="upload__text" component="h6" variant="h6">Выберите фото профиля</Typography>
+                     <Typography className="upload__tip" component="span" variant="h6">Перетяните изображение сюда или кликните</Typography>
+                        <DropzoneArea
+                           onChange={this.handleUpload.bind(this)}
+                        />
+                     </Grid>
+                     <Grid className="grid__item" item xs={12}>
+                        <Button type="submit" variant="contained" color="primary">Сохранить</Button>
+                     </Grid>
+                  </Grid>
+               </form>
+            </Paper>
+         </div>
+      )
+   }
+}
+
+ProfileEditForm.propTypes = {
+   classes: PropTypes.object.isRequired,
+};
+export default withStyles(styles)(ProfileEditForm);
--- src/Ziro/Ziro.Web/wwwroot/react-dev/components/Profile/index.jsx
+++ src/Ziro/Ziro.Web/wwwroot/react-dev/components/Profile/index.jsx
@@ -4,86 +4,132 @@ import Paper from '@material-ui/core/Paper';
 import Typography from '@material-ui/core/Typography';
 import Button from '@material-ui/core/Button';
 import Icon from '@material-ui/core/Icon';
-import AddIcon from '@material-ui/icons/Add';
-import SvgIcon from '@material-ui/core/SvgIcon';
-
-import { blue, red, yellow, purple, green } from '@material-ui/core/colors';
 import withStyles from '@material-ui/core/styles/withStyles';
 import ava from '../../img/ava_e-v.jpg'
 import './profile.css'
 import { isUserAuthenticated } from '../../utils.js'
+import EditForm from './ProfileEditForm.jsx'
+import { fetchGetData } from '../../utils.js';
 
 const styles = theme => ({
-    paper: {
-       ' max-width': '900px',
-        marginLeft: 'auto',
-        marginRight: 'auto',
-        //spacing.unit = 8
-        padding: `${theme.spacing.unit * 4}px ${theme.spacing.unit * 5}px ${theme.spacing.unit * 5}px`,
-    },
-    ava__wrap: {
-        width: '150px',
-        height: '150px',
-    },
-    ava: {
-        width: '100%',
-        borderRadius: '50%',
-    },
-    customCss: {
-        // color: theme.palette.getContrastText(purple[300]),
-        color: '#fff',
-        backgroundColor: blue[700],
-        '&:hover': {
-          backgroundColor: blue[400],
-        },
-        padding: '6px 12px',
-        margin: '10px 0'
-    }
+   paper: {
+      ' max-width': '900px',
+      marginLeft: 'auto',
+      marginRight: 'auto',
+      //spacing.unit = 8
+      padding: `${theme.spacing.unit * 4}px ${theme.spacing.unit * 5}px ${theme.spacing.unit * 5}px`,
+   },
+   ava__wrap: {
+      width: '150px',
+      height: '150px',
+   },
+   ava: {
+      width: '100%',
+      borderRadius: '50%',
+   },
+   editBtn: {
+      marginTop: '10px'
+   }
+
 });
 
 
 class Profile extends Component {
-	constructor(props) {
-		super(props);
-		if (!isUserAuthenticated()) {
-			this.props.history.push('/authorization');
-			return;
-		}
-	}
-    render() {
-        const { classes } = this.props;
-        return (
-            <div className="container">
-            <Paper className={`${classes.paper} profile__wrap`}>
-                <div className="profile__image-block col">
-                    <div className={classes.ava__wrap}>
-                        <img className={classes.ava} src={ava} alt="user avatar"/>
-                    </div>
-                    <Button variant="contained" className={`${classes.customCss} profile__btn`}>Редактировать
-                    <Icon>edit_icon</Icon>                        
-                    </Button>
-                </div>
-                <div className="profile__info-block col">
-                    <Typography className="profile__name" color="textPrimary" component="p" variant="h5">Вихарев Егор</Typography>
-                    <Typography className="profile__position" color="textPrimary" component="p" variant="p">ASP.NET developer</Typography>
-                    <br/>
-                    <Typography className="profile__skills" color="textPrimary" component="p" variant="p">Technology and skills: <span>C#, ASP.NET, DB, javascript</span></Typography>
-                    <br/>
-                    <Typography className="profile__projects" color="textPrimary" component="p" variant="p">Current projects: <span>Ziro, Machine learning startap</span></Typography>
-                </div>
-                <div className="profile__exit col">
-                    <Button variant="contained" className={`${classes.customCss} profile__btn`}>Выйти
-                        <Icon>open_in_new</Icon>                        
-                    </Button>
-                </div>
-            </Paper>
-            </div>
-        );
+   constructor(props) {
+      super(props);
+      if (!isUserAuthenticated()) {
+         this.props.history.push('/authorization');
+         return;
       }
+      this.state = {
+         isEditFormVisible: false,
+         name: null,
+         lastName: null,
+         email: null,
+         skype: null,
+         phoneNumber: null,
+         position: null,
+         dateOfBirth: null,
+         data: null,
+      };
+      this.editFormSetVisibility = this.editFormSetVisibility.bind(this);
+      this.successGetData = this.successGetData.bind(this);
+      this.errorGetData = this.errorGetData.bind(this);
+      fetchGetData('api/user/getProfile', this.successGetData, this.errorGetData);
+   }
+
+   successGetData(response){
+      var data = response.data;
+      if (!response.errors) {
+         this.setState({
+            name: data.name,
+            lastName: data.lastName,
+            email: data.email,
+            skype: data.skype,
+            phoneNumber: data.phoneNumber, 
+            position: data.position.name, 
+            dateOfBirth: data.dateOfBirth,
+            data: data
+         })
+      }
+   }
+
+   errorGetData(error) {
+      alert(error);
+   }
+
+   editFormSetVisibility(e) {
+      e.preventDefault();
+      this.setState({ isEditFormVisible: !this.state.isEditFormVisible });
+   }
+
+   logout = () => {
+      this.props.history.push('/logout');
+   }
+
+   render() {
+      const { classes } = this.props;
+      return (
+         <div className="container">
+            <Paper className={`${classes.paper} profile__wrap`}>
+               <div className="profile__image-block col">
+                  <div className={classes.ava__wrap}>
+                     <img className={classes.ava} src={ava} alt="user avatar" />
+                  </div>
+                  <Button variant="contained" color="primary" className={`${classes.editBtn} profile-edit__btn`} onClick={this.editFormSetVisibility}>
+                  {this.state.isEditFormVisible ? 'Отмена' : 'Редактировать'}
+                    <Icon>edit_icon</Icon>
+                  </Button>
+               </div>
+               <div className="profile__info-block col">
+                  <Typography className="profile__name" color="textPrimary" component="h1" variant="h5">{this.state.lastName} {this.state.name}</Typography>
+                  <Typography className="profile__position" color="textPrimary" component="h2" variant="h6">{this.state.position}</Typography>
+                  <br />
+                  <p className="personal-data">email: {this.state.email}</p>
+                  <p className="personal-data">skype: {this.state.skype}</p>
+                  <p className="personal-data">Номер телефона: {this.state.phoneNumber}</p>
+                  <p className="personal-data">Дата рождения: {this.state.dateOfBirth}</p>
+
+                  <p className="personal-data">Technology and skills: <span>C#, ASP.NET, DB, javascript</span></p>
+                  <p className="personal-data">Current projects: <span>Ziro, Machine learning startap</span></p>
+               </div>
+               <div className="profile__exit col">
+                  <Button variant="contained" color="primary" onClick={this.logout}>
+                     Выйти
+                        <Icon>open_in_new</Icon>
+                  </Button>
+               </div>
+            </Paper>
+
+            {this.state.isEditFormVisible ? <EditForm data={this.state.data}/> : null}
+
+         </div>
+      );
+   }
 }
 
 Profile.propTypes = {
-    classes: PropTypes.object.isRequired,
+   classes: PropTypes.object.isRequired,
 };
 
 export default withStyles(styles)(Profile);
